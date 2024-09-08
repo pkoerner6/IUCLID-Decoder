3a5ae5e590cb5f7998ff0035755d733a5c678144
Paulina Koerner <paulina.koerner@gmail.com>
2024-09-08 16:54:43 +0200

    testing overflow

--- iuclid_decoder/iuclid_parser_utils.py
+++ iuclid_decoder/iuclid_parser_utils.py
@@ -716,10 +716,17 @@ def convert_units_given_unit_dicts(df: pd.DataFrame, value_col: str, unit_col: s
                 if unit in unit_to_calc: # Get the conversion factor for the unit
                     calc = unit_to_calc[unit]
                     max_value = np.finfo(np.float64).max # TODO
-                    if calc > 1 and value_list[index] > max_value / calc:
-                        print("value_list[index]: ", value_list[index]) # TODO
+                    # if calc > 1 and value_list[index] > max_value / calc:
+                        # print("value_list[index]: ", value_list[index]) # TODO
+                        # print("calc: ", calc)
+                    try:
+                        value_list[index] = value_list[index] * calc # TODO
+                    except RuntimeWarning as e:
+                        print("Overflow caught!")
+                        print("value_list[index]: ", value_list[index])
                         print("calc: ", calc)
-                    value_list[index] = value_list[index] * calc # TODO
+
+                    
                     unit_list[index] = unit_to_new_unit[unit] # Update the unit to the new unit name
             # Update the DataFrame with the converted values and units
             df[val_col] = df[val_col].astype(str)
